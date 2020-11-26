0dc88ae2d81e932690b678eec213cea89bccf1a1
anniesnoopymd <anniesnoopymd@gmail.com>
2020-11-26 10:42:16 +0800

    edit labels simulation

--- IC_modulation.R
+++ IC_modulation.R
@@ -8,26 +8,31 @@ pbad.irreg=0.4
 pgood.irreg=1-pbad.irreg
 
 sim.ICmodule = function(n=10){
-  simulated.modules = c()
-  lebels = sample(c(-1,1),1,
+  simulated.modules = rep(NA,n)
+  labels = sample(c(-1,1),n,
                   prob=c(pirreg,preg),
                   replace=T)
   
   if(any(labels==1)){
-    simulated.modules[which(labels==1)] = 
-      sample(c("goodreg","badreg"),n,
+    simulated.modules[which(labels==1)]=
+      sample(c("goodreg","badreg"), #從reg中拿出來是好(good)的或壞(bad)的
              sum(labels==1),
              prob=c(pgood.reg,pbad.reg),
-             replace=T)
-  }else{
+             replace=TRUE) 
+  }
+  if(any(labels==-1)){
     simulated.modules[which(labels==-1)] = 
-      sample(c(-1,1),n,
+      sample(c("goodirreg","badirreg"),
              sum(labels==-1),
-             prob=c(pbad.irreg,pgood.irreg),
+             prob=c(pgood.irreg,pbad.irreg),
              replace=T)
   }
   simulated.modules
 }
 
-ICmodule.sim()
+sim.ICmodule()
 S=10000
+sim.table = replicate(S,sim.ICmodule())
+dim(sim.table)
+
+
