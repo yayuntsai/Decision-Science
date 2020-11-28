9da11b163e85a033458ea40aa46b7e43b6c089f3
anniesnoopymd <anniesnoopymd@gmail.com>
2020-11-28 20:08:29 +0800

    HW1 Q3 expected num

--- HW1.R
+++ HW1.R
@@ -11,5 +11,34 @@ for(s in 1:S){
                  replace=T)
   smallest[s] =  min(dice1[s],dice2[s],dice3[s])
 }
-exp_smallest = sum(smallest)/S
+prob.table = table(smallest)/S
+exp_smallest = 0
+for(i in 1:6){
+  exp_smallest = exp_smallest + i * as.numeric(prob.table)[i]
+}
+
 exp_smallest
+
+
+
+
+#Q4
+
+
+
+
+
+
+#Q5
+
+n=1000
+total.days=c()
+for(s in 1:n){
+  borrow.days = sample(c(4,5,6,7),n,
+                       prob=c(0.1,0.2,0.3,0.4),
+                       replace=T)
+  delay = sample(c(1,0),n,
+                 replace=T)
+  total.days[s] = borrow.days[s] + 2 * delay[s]
+}
+prob.wed = sum(total.days=6)/n
